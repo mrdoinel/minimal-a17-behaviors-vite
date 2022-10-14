dcba4c66c498a44dc048765ae13c0292a381f53b
Antoine Doury <antoine@area17.com>
2022-10-14 19:32:20 +0200

    Trying to figure out how Vite Dynamic import is working :)

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { resolve } from 'path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import environmentPlugin from 'vite-plugin-environment'
+import dynamicImport from 'vite-plugin-dynamic-import'
 
 export default ({ mode }) =>
     defineConfig({
         plugins: [
             environmentPlugin({
                 MODE: mode,
-                BEHAVIORS_PATH: resolve(
-                    __dirname,
-                    'js/behaviors'
-                ),
+                BEHAVIORS_PATH: fileURLToPath(new URL('js/behaviors', import.meta.url)),
                 BEHAVIORS_EXTENSION: 'js'
-            })
+            }),
+            dynamicImport()
         ],
         resolve: {
             alias: {
